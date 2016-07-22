e2e126abe998072e2692265af8998e65c7c353fe
sepehriqani <sepehr_e2008@yahoo.com>
2016-07-22 23:44:56 +0430

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace abstract_class
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var s = new mojodat.tavos
+            {
+                name = m
+            }
+        }
+    }
+}
